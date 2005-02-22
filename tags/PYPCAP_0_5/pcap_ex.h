910a032a00cf3ae0aabbae31b52032ec013e3d14
dugsong <dugsong@0225ec8c-8f27-0410-80df-577ead3473a2>
2005-02-22 05:07:09 +0000

    This commit was manufactured by cvs2svn to create tag 'PYPCAP_0_5'.

---
+++ tags/PYPCAP_0_5/pcap_ex.h
@@ -0,0 +1,13 @@
+/* $Id$ */
+
+#ifndef PCAP_EX_H
+#define PCAP_EX_H
+
+void  pcap_ex_immediate(pcap_t *pcap);
+char *pcap_ex_name(char *name);
+char *pcap_ex_lookupdev(char *ebuf);
+int   pcap_ex_fileno(pcap_t *pcap);
+void  pcap_ex_setup(pcap_t *pcap);
+int   pcap_ex_next(pcap_t *pcap, struct pcap_pkthdr **hdr, u_char **pkt);
+
+#endif /* PCAP_EX_H */
